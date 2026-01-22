ca4f69fa772f33f5b057889a187b2e8a2fadaa76
Oskar Wickström <oskar@wickstrom.tech>
2026-01-22 14:04:06 +0100

    run integration tests in CI
    * run integration tests as checkPhase in the executable
    * configure insta to work in sandboxed nix build

--- nix/executable.nix
+++ nix/executable.nix
@@ -1,11 +1,32 @@
-{ callPackage, rustPlatform, pkg-config, esbuild, chromium, }:
+{
+  callPackage,
+  rustPlatform,
+  pkg-config,
+  esbuild,
+  chromium,
+}:
 let
-  customBuildRustCrateForPkgs = pkgs:
+  customBuildRustCrateForPkgs =
+    pkgs:
     pkgs.buildRustCrate.override {
       defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-        bombadil = attrs: { nativeBuildInputs = [ esbuild ]; };
+        bombadil = attrs: {
+          nativeBuildInputs = [ esbuild ];
+        };
       };
     };
-in (callPackage ./Cargo.nix {
+in
+(callPackage ./Cargo.nix {
   buildRustCrateForPkgs = customBuildRustCrateForPkgs;
-}).rootCrate.build
+}).rootCrate.build.override
+  {
+    runTests = true;
+    testInputs = [ chromium ];
+    testPreRun = ''
+      export XDG_CONFIG_HOME=$(mktemp -d)
+      export XDG_CACHE_HOME=$(mktemp -d)
+
+      export INSTA_WORKSPACE_ROOT=$CARGO_MANIFEST_DIR
+      export INSTA_UPDATE=no
+    '';
+  }
