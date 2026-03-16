a47ee6603afb5a6ada9fc6dbc66c35907bf79876
Oskar Wickström <oskar@wickstrom.tech>
2026-03-16 08:42:45 +0100

    cpu and heap in bottom

--- lib/bombadil-inspect/src/lib.rs
+++ lib/bombadil-inspect/src/lib.rs
@@ -119,102 +119,75 @@ fn app() -> Html {
                 <h2>{"State after"}</h2>
                 <div class="todo">{"TODO: screenshot"}</div>
             </div>
-            <div class="pane state-space">
-                <h2>{"State space"}</h2>
-            </div>
-            <div class="pane heap">
-                <h2>{"Heap"}</h2>
-
-                <svg viewBox="0 0 600 300" xmlns="http://www.w3.org/2000/svg" style="width:100%;height:auto;">
-                <polyline
-                fill="none"
-                stroke="#000"
-                stroke-width="2"
-                points="
-                     0,280
-                     40,280
-                     40,260
-                     90,260
-                     90,230
-                     120,230
-                     120,220
-                     180,220
-                     180,190
-                     200,190
-                     200,170
-                     260,170
-                     260,140
-                     280,140
-                     280,110
-                     330,110
-                     330,80
-                     350,80
-                     350,90
-                     400,90
-                     400,70
-                     430,70
-                     430,50
-                     460,50
-                     460,40
-                     510,40
-                     510,50
-                     540,50
-                     540,30
-                     600,30
-                     "
-                />
-                </svg>
-            </div>
-            <div class="pane cpu">
-                <h2>{"CPU"}</h2>
-                <svg viewBox="0 0 600 300" xmlns="http://www.w3.org/2000/svg" style="width:100%;height:auto;">
-                <path
-                fill="none"
-                stroke="#000"
-                stroke-width="2"
-                d="M 0,160
-                L 15,145 25,155 40,120
-                C 60,60 70,40 100,45
-                L 115,55 125,35 135,50
-                C 155,90 165,130 185,170
-                L 195,190 205,175 215,200
-                C 235,250 255,270 280,265
-                L 290,255 300,270 310,250
-                C 330,200 340,150 360,110
-                L 370,95 380,105 390,80
-                C 410,40 430,35 455,50
-                L 465,60 475,42 485,55
-                C 500,100 510,150 530,190
-                L 540,210 548,195 555,215
-                C 570,250 580,260 600,245"
-                />
-                </svg>
-            </div>
-            <div class="pane statistics">
-                <h2>{"Statistics"}</h2>
-                <div class="content">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th class="right">{"New"}</th>
-                                <th class="right">{"Total"}</th>
-                            </tr>
-                        </thead>
-                    {
-                        statistics.iter().map(|(label, new, total)| {
-                            html!{
-                                <tr>
-                                    <td>{label}</td>
-                                    <td class="right">{new}</td>
-                                    <td class="right">{total}</td>
-                                </tr>
-                            }
-                        }).collect::<Html>()
-                    }
-                    </table>
+                <footer class="pane">
+                <div class="heap">
+                    <header><h2>{"Heap"}</h2></header>
+                    <svg viewBox="0 0 600 30" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+                    <polyline
+                    fill="none"
+                    stroke="#000"
+                    stroke-width=".5"
+                    points="
+                    0,28
+                    40,28
+                    40,26
+                    90,26
+                    90,23
+                    120,23
+                    120,22
+                    180,22
+                    180,19
+                    200,19
+                    200,17
+                    260,17
+                    260,14
+                    280,14
+                    280,11
+                    330,11
+                    330,8
+                    350,8
+                    350,9
+                    400,9
+                    400,7
+                    430,7
+                    430,5
+                    460,5
+                    460,4
+                    510,4
+                    510,5
+                    540,5
+                    540,3
+                    600,3
+                    "
+                    />
+                    </svg>
+                    </div>
+                    <div class="cpu">
+                        <header><h2>{"CPU"}</h2></header>
+                        <svg viewBox="0 0 600 30" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+                        <path
+                        fill="none"
+                        stroke="#000"
+                        stroke-width=".5"
+                        d="M 0,16
+                        L 15,14 25,15 40,12
+                        C 60,6 70,4 100,4
+                        L 115,5 125,3 135,5
+                        C 155,9 165,13 185,17
+                        L 195,19 205,17 215,20
+                        C 235,25 255,27 280,26
+                        L 290,25 300,27 310,25
+                        C 330,20 340,15 360,11
+                        L 370,9 380,10 390,8
+                        C 410,4 430,3 455,5
+                        L 465,6 475,4 485,5
+                        C 500,10 510,15 530,19
+                        L 540,21 548,19 555,21
+                        C 570,25 580,26 600,24"
+                        />
+                    </svg>
                 </div>
-            </div>
+            </footer>
         </main>
     }
 }
