dff406bfb91339d1366bd8bf1617184b15c18ed7
Mohit Karekar <karekar.mohit@gmail.com>
2026-06-30 00:40:55 +0200

    Rebase with main, make action functions async

--- lib/bombadil/src/specification/browser/index.ts
+++ lib/bombadil/src/specification/browser/index.ts
@@ -216,7 +216,7 @@ function getStructuralPath(el: Element): string {
 
 export function registerCustomAction(
   name: string,
-  scriptFunction: (state: Pick<State, "document" | "window">) => void,
+  scriptFunction: (state: Pick<State, "document" | "window">) => Promise<void>,
 ) {
   bombadil.registerCustomAction<Pick<State, "document" | "window">>(
     name,
