fd2da83d373501c71e990fc8a641800d3462e468
Oskar Wickström <oskar@wickstrom.tech>
2026-06-30 11:55:53 +0200

    improve nano spec

--- examples/nano.ts
+++ examples/nano.ts
@@ -6,19 +6,22 @@ import {
   typeFromSet,
 } from "@antithesishq/bombadil/terminal/defaults/actions";
 
-const statusLinesWords: Cell<string[]> = extract((state) => {
-  if (state.grid.size.rows < 2) {
-    return [];
-  }
-  const line1 = state.grid.rowText(state.grid.size.rows - 2);
-  const line2 = state.grid.rowText(state.grid.size.rows - 1);
-  return [line1, line2]
-    .flatMap((line) => line.split(/\s+/))
-    .map((word) => word.trim())
-    .filter(Boolean);
-  // .filter((word) => !word.match(/^(:?M-\w|\^.)$/));
-});
+// const statusLinesText: Cell<string> = extract((state) => {
+//   const { rows } = state.grid.size;
+//   if (rows < 2) return "";
+//   return state.grid.rowText(rows - 2) + " " + state.grid.rowText(rows - 1);
+// });
 
+// export const hasStandardBindings = always(
+//   next(() => {
+//     const text = statusLinesText.current ?? "";
+//     return (
+//       justExited() ||
+//       (text.includes("Help") && text.includes("Exit") && text.includes("Read"))
+//     );
+//   }),
+// );
+//
 function justExited(): boolean {
   return (
     !!lastAction.current &&
@@ -28,32 +31,8 @@ function justExited(): boolean {
   );
 }
 
-// export const hasStandardBindings = always(
-//   now(() => !justExited())
-//     .and(next(() => !justExited()))
-//     .implies(
-//       next(
-//         next(() => {
-//           const words = new Set(statusLinesWords.current ?? []);
-//           return (
-//             justExited() ||
-//             ["Help", "Exit", "Read"].every((word) => words.has(word))
-//           );
-//         }),
-//       ),
-//     ),
-// );
-
-export const hasStandardBindings = always(
-  next(() => {
-    const words = new Set(statusLinesWords.current ?? []);
-    return (
-      justExited() || ["Help", "Exit", "Read"].every((word) => words.has(word))
-    );
-  }),
-);
-
 export const typeRandom = weighted([
   [40, typeFromSet(CharSets.UNICODE_SAFE)],
   [40, typeFromSet(CharSets.CONTROL_COMMON)],
+  [1, { Resize: { rows: [10, 100], columns: [40, 120] } }],
 ]);
