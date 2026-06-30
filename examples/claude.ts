5bace3ed0caafe53bb88f5075177e6b218c6e3c0
Oskar Wickström <oskar@wickstrom.tech>
2026-06-30 11:33:58 +0200

    document claude spec

--- examples/claude.ts
+++ examples/claude.ts
@@ -1,4 +1,5 @@
-import { ActionGenerator, CharSet, leaf } from "@antithesishq/bombadil/actions";
+/// This is a spec for Claude Code, the TUI coding agent from Anthropic.
+import { ActionGenerator, CharSet } from "@antithesishq/bombadil/actions";
 import { actions, extract, weighted } from "@antithesishq/bombadil/terminal";
 import { CharSets } from "@antithesishq/bombadil/terminal/defaults/actions";
 import { typeFromSet } from "@antithesishq/bombadil/terminal/defaults/actions";
@@ -6,7 +7,6 @@ import { typeFromSet } from "@antithesishq/bombadil/terminal/defaults/actions";
 const ui = extract((state) => {
   let working = false;
   for (let index = state.grid.size.rows - 1; index >= 0; index--) {
-    // const text = state.grid.rowText(index).trim();
     const cells = state.grid.row(index);
     working =
       working ||
@@ -20,14 +20,21 @@ const ui = extract((state) => {
 
 export const typeRandom = new ActionGenerator(() =>
   weighted([
-    // noop
+    // Noop, most likely when Claude says it's working, to give it a chance to
+    // finish. We do still allow for input, which usually means queued messages.
     [ui.current?.working ? 1000 : 10, typeFromSet(CharSet.fromLiterals(""))],
+
+    // Otherwise, we hammer on with various inputs and control sequences.
     [40, typeFromSet(CharSets.UNICODE_SAFE)],
     [40, typeFromSet(CharSets.ASCII_PRINTABLE)],
     [40, typeFromSet(CharSets.CONTROL_ALL)],
+
+    // Submit message.
+    [5, typeFromSet(CharSet.fromLiterals("\r\n"))],
+
+    // We also scroll and resize the terminal.
     [5, { ScrollUp: {} }],
     [1, { ScrollDown: {} }],
-    [5, typeFromSet(CharSet.fromLiterals("\r\n"))],
     [
       1,
       actions(() => [
