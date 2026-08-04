06d18224d4ae0d8bb0ff97ff5eece5cc0d518605
Oskar Wickström <oskar@wickstrom.tech>
2026-08-04 15:04:45 +0200

    put init function in proper section on macos

--- lib/antithesis-coverage/src/lib.rs
+++ lib/antithesis-coverage/src/lib.rs
@@ -11,7 +11,13 @@ unsafe extern "C" {
 }
 
 #[used]
-#[unsafe(link_section = ".init_array")]
+// https://refspecs.linuxbase.org/LSB_3.0.0/LSB-PDA/LSB-PDA/specialsections.html
+#[cfg_attr(target_os = "linux", unsafe(link_section = ".init_array"))]
+// https://github.com/aidansteele/osx-abi-macho-file-format-reference#table-2-the-sections-of-a__datasegment
+#[cfg_attr(
+    target_os = "macos",
+    unsafe(link_section = "__DATA,__mod_init_func")
+)]
 static _ANTITHESIS_INIT: unsafe extern "C" fn() = antithesis_load_libvoidstar;
 
 pub fn init_coverage_module(
